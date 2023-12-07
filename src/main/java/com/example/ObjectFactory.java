a0a9141f23f064901e64e0997944a20974ee8922
varrivoda <varrivoda@yandex.ru>
2023-12-07 10:04:56 +1000

    5. Object tuning, part 2: parsing property file

--- src/main/java/com/example/ObjectFactory.java
+++ src/main/java/com/example/ObjectFactory.java
@@ -2,9 +2,17 @@ package com.example;
 
 import lombok.SneakyThrows;
 
+import javax.naming.CannotProceedException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.annotation.Annotation;
 import java.lang.invoke.SerializedLambda;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
 
 public class ObjectFactory {
     private static final ObjectFactory $$instance = new ObjectFactory();
@@ -22,6 +30,27 @@ public class ObjectFactory {
         }
         Object t =implClass.getDeclaredConstructor().newInstance();
 
+
+        for (Field field : implClass.getDeclaredFields()) {
+            InjectProperty annotation = field.getAnnotation(InjectProperty.class);
+
+            //Convert properties file to protertiesMap
+            String path = ClassLoader.getSystemClassLoader().getResource("application.properties").getPath();
+            Stream<String> lines = new BufferedReader(new FileReader(path)).lines();
+            Map<String, String> propertiesMap = lines
+                    .map(line -> line.split("="))
+                    //stupit splitting, works only without whitespaces
+                    .collect(toMap(arr -> arr[0], arr -> arr[1]));
+
+            if(annotation!=null){
+                //если ваннотации было значение,берем его. ≈сли не юбыло, берем из файла ѕроперти
+                String value = annotation.value().isEmpty() ? propertiesMap.get(field.getName()) : propertiesMap.get(annotation.value());
+
+                // теперь знаение надо засетить в поле
+                field.setAccessible(true);
+                field.set(t, value);
+            }
+        }
         return (T) t;
     }
 }
